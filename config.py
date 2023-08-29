9b943e6e2e68ce63e359f4ac9316c68723354bb1
Useless <91428127+Dimanoname@users.noreply.github.com>
2023-08-29 13:13:35 +0300

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-botik = "token" # BOT TOKEN
-admin = admin_id # Admin id, ex. 1234567, 12345678, 123456789 or 12345678
-welcoming = "Welcome to advertisement bot!" # Welcome message
-cmd = "send" # Command
-symb = len(cmd)+2 # DO NOT TOUCH
