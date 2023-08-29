754b031f0400b4bbf6f76c8e16a5a74b15fde6a4
Useful <86927375+Useleness@users.noreply.github.com>
2023-08-29 04:16:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 botik = "token" # BOT TOKEN
 admin = admin_id # Admin id, ex. 1234567, 12345678, 123456789 or 12345678
-welcoming = "Привет, это бот для рекламы" # Welcome message
+welcoming = "Welcome to advertisement bot!" # Welcome message
 cmd = "send" # Command
 symb = len(cmd)+2 # DO NOT TOUCH
